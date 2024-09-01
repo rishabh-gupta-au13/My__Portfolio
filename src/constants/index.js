e783461c147b177b362b91620db91d4e3d21df65
Jai Sharma <jai.sharma@universityliving.com>
2024-09-01 14:21:44 +0530

    updated the projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,6 @@
 import { universityLivingLogo } from "../assets/images";
 import {
-  car,
   css,
-  estate,
   express,
   git,
   github,
@@ -13,14 +11,10 @@ import {
   mui,
   nextjs,
   nodejs,
-  pricewise,
   react,
   redux,
   sass,
-  snapgram,
-  summiz,
   tailwindcss,
-  threads,
   typescript,
   shadcn,
 } from "../assets/icons";
@@ -167,51 +161,12 @@ export const socialLinks = [
 
 export const projects = [
   {
-    iconUrl: pricewise,
+    icon: "IoChatboxEllipsesOutline",
     theme: "btn-back-red",
-    name: "Amazon Price Tracker",
+    name: "Chat Pdf AI",
     description:
-      "Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.",
-    link: "https://github.com/adrianhajdin/pricewise",
-  },
-  {
-    iconUrl: threads,
-    theme: "btn-back-green",
-    name: "Full Stack Threads Clone",
-    description:
-      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-    link: "https://github.com/adrianhajdin/threads",
-  },
-  {
-    iconUrl: car,
-    theme: "btn-back-blue",
-    name: "Car Finding App",
-    description:
-      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
-    link: "https://github.com/adrianhajdin/project_next13_car_showcase",
-  },
-  {
-    iconUrl: snapgram,
-    theme: "btn-back-pink",
-    name: "Full Stack Instagram Clone",
-    description:
-      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-    link: "https://github.com/adrianhajdin/social_media_app",
-  },
-  {
-    iconUrl: estate,
-    theme: "btn-back-black",
-    name: "Real-Estate Application",
-    description:
-      "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
-    link: "https://github.com/adrianhajdin/projects_realestate",
-  },
-  {
-    iconUrl: summiz,
-    theme: "btn-back-yellow",
-    name: "AI Summarizer Application",
-    description:
-      "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
-    link: "https://github.com/adrianhajdin/project_ai_summarizer",
+      "Create a personalized chatbot using your PDF as context! Built with Next.js, Pinecone, and Google Gemini, this web app transforms your document into an interactive, AI-powered chat assistant, making it easy to retrieve and engage with the content of your PDF.",
+    githubLink: "https://github.com/JaiSharma1710/chat_pdf",
+    link: "https://chatpdf.jaisharma.co.in/",
   },
 ];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
 
 import { CTA } from "../components";
 import { projects } from "../constants";
 import { arrow } from "../assets/icons";
 
+const Icons = {
+  IoChatboxEllipsesOutline: (
+    <IoChatboxEllipsesOutline className="w-8 text-white h-8" />
+  ),
+};
+
 const Projects = () => {
   return (
-    <section className='max-container'>
-      <h1 className='head-text'>
+    <section className="max-container">
+      <h1 className="head-text">
         My{" "}
-        <span className='blue-gradient_text drop-shadow font-semibold'>
+        <span className="blue-gradient_text drop-shadow font-semibold">
           Projects
         </span>
       </h1>
 
-      <p className='text-slate-500 mt-2 leading-relaxed'>
+      <p className="text-slate-500 mt-2 leading-relaxed">
         I've embarked on numerous projects throughout the years, but these are
         the ones I hold closest to my heart. Many of them are open-source, so if
         you come across something that piques your interest, feel free to
@@ -22,46 +30,54 @@ const Projects = () => {
         Your collaboration is highly valued!
       </p>
 
-      <div className='flex flex-wrap my-20 gap-16'>
+      <div className="flex flex-wrap my-20 gap-16">
         {projects.map((project) => (
-          <div className='lg:w-[400px] w-full' key={project.name}>
-            <div className='block-container w-12 h-12'>
+          <div
+            className="lg:w-[400px] w-full shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] p-4 rounded-lg"
+            key={project.name}
+          >
+            <div className="block-container w-12 h-12">
               <div className={`btn-back rounded-xl ${project.theme}`} />
-              <div className='btn-front rounded-xl flex justify-center items-center'>
-                <img
-                  src={project.iconUrl}
-                  alt='threads'
-                  className='w-1/2 h-1/2 object-contain'
-                />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                {Icons[project.icon]}
               </div>
             </div>
 
-            <div className='mt-5 flex flex-col'>
-              <h4 className='text-2xl font-poppins font-semibold'>
+            <div className="mt-5 flex flex-col">
+              <h4 className="text-2xl font-poppins font-semibold">
                 {project.name}
               </h4>
-              <p className='mt-2 text-slate-500'>{project.description}</p>
-              <div className='mt-5 flex items-center gap-2 font-poppins'>
+              <p className="mt-2 text-slate-500">{project.description}</p>
+              <div className="mt-5 flex justify-between items-center gap-2 font-poppins">
+                <div className="flex items-center gap-2">
+                  <Link
+                    to={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="font-semibold text-blue-600"
+                  >
+                    Live Link
+                  </Link>
+                  <img
+                    src={arrow}
+                    alt="arrow"
+                    className="w-4 h-4 object-contain"
+                  />
+                </div>
                 <Link
-                  to={project.link}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='font-semibold text-blue-600'
+                  to={project.githubLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  Live Link
+                  <FaGithub className="w-8 h-8 cursor-pointer" />
                 </Link>
-                <img
-                  src={arrow}
-                  alt='arrow'
-                  className='w-4 h-4 object-contain'
-                />
               </div>
             </div>
           </div>
         ))}
       </div>
 
-      <hr className='border-slate-200' />
+      <hr className="border-slate-200" />
 
       <CTA />
     </section>
